8dfd520e1d73092dd58ced79859655af501fceb5
jose <2130162@my.ipleiria.pt>
2017-05-07 21:04:44 +0100

    melhoramentos add-edit

--- resources/views/print_requests/partials/add-edit.blade.php
+++ resources/views/print_requests/partials/add-edit.blade.php
@@ -1,4 +1,5 @@
 {{csrf_field()}}
+
 <div class="form-group">
     <label for="inputDescription">Description</label>
     <input
@@ -7,10 +8,8 @@
         placeholder="description"
         @if(Route::currentRouteName()=='requests.store')
         value="{{old('description', $request->description)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->description}}"
-        @else
-        value=""
         @endif
     />
 </div>
@@ -19,13 +18,11 @@
 <label for="inputRequestDate">Request Date</label>
     <input
         type="date" class="form-control"
-        name="request_date" id="inputRequestDate"
+        name="request_date" id="inputRequestDate" 
         @if(Route::currentRouteName()=='requests.store')
         value="{{old('request_date', $request->request_date)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->request_date}}"
-        @else
-        value=""
         @endif
     />
 </div>
@@ -37,10 +34,8 @@
         name="due_date" id="inputDueDate"
         @if(Route::currentRouteName()=='requests.store')
         value="{{old('due_date', $request->due_date)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->due_date}}"
-        @else
-        value=""
         @endif
     />
 </div>
@@ -50,13 +45,11 @@
     <input
         type="number" class="form-control"
         name="quantity" id="inputQuantity"
-        placeholder="quantity"
+        placeholder="quantity" min="1" 
         @if(Route::currentRouteName()=='requests.store')
         value="{{old('quantity', $request->quantity)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->quantity}}"
-        @else
-        value=""
         @endif
     />
 </div>
@@ -66,11 +59,9 @@
     <select name="colored" id="inputColored" class="form-control "
     @if(Route::currentRouteName()=='requests.store')
         value="{{old('quantity', $request->quantity)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->quantity}}"
-        @else
-        value=""
-        @endif">
+        @endif
         <option disabled selected> -- select an option -- </option>
         <option value="0">Color</option>
         <option value="1">Black and White</option>
@@ -82,10 +73,8 @@
     <select name="stapled" id="inputStapled" class="form-control "
     @if(Route::currentRouteName()=='requests.store')
         value="{{old('stapled', $request->stapled)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->stapled}}"
-        @else
-        value=""
         @endif">
         <option disabled selected> -- select an option -- </option>
         <option value="0">With Staple</option>
@@ -98,10 +87,8 @@
     <select name="paper_size" id="inputPaperSize" class="form-control "
     @if(Route::currentRouteName()=='requests.store')
         value="{{old('paper_size', $request->paper_size)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->paper_size}}"
-        @else
-        value=""
         @endif">
         <option disabled selected> -- select an option -- </option>
         <option value="0">A4</option>
@@ -114,10 +101,8 @@
     <select name="paper_type" id="inputPaperType" class="form-control "
     @if(Route::currentRouteName()=='requests.store')
         value="{{old('paper_type', $request->paper_type)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->paper_type}}"
-        @else
-        value=""
         @endif">
         <option disabled selected> -- select an option -- </option>
         <option value="0">Draft</option>
@@ -130,13 +115,12 @@
     <label for="inputFile">File</label>
     <input
         type="file" class="form-control"
-        name="file" id="inputFile" accept="image/png"
+        name="file" id="inputFile" accept="image/*, application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, .csv"
+
         @if(Route::currentRouteName()=='requests.store')
         value="{{old('file', $request->file)}}"
-        @elsif(Route::currentRouteName()=='requests.edit')
+        @else(Route::currentRouteName()=='requests.edit')
         value="{{$request->file}}"
-        @else
-        value=""
         @endif
     />
 </div>
