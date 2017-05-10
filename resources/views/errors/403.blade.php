c515ac829c958bc3d6180282b44311f57cc31f20
Marcelo Carvalho <ineyve17@hotmail.com>
2017-05-10 09:14:45 +0100

    404 error now animated

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">403 - Forbidden</div>
                 <div class="panel-body">
-                    <div class="home-counter"><h2>You are not allowed in this page</h2></div>
+                    <div class="home-counter"><h2>You are not allowed in this page.</h2></div>
                     <br><br><br><br><br><br>
                     <header class="header"><h1 class="glitched">403</h1></header>
                 </div>
