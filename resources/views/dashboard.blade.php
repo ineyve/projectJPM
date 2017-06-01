8e7389344eda1bb52a46d599d0959f05d7ea5328
xQsme <2151163@my.ipleiria.pt>
2017-06-01 13:26:56 +0100

    stuff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,48 +11,13 @@
                         @else
                             {{$user->name}}'s
                         @endif
-                        Profile (User {{$user->id}})
+                        Dashboard (User {{$user->id}})
                     </div>
                     <div class="panel-body">
-                        <div class="profile">
-                            @if(is_null($user->profile_photo))
-                                <img class="profile-picture" src="/profile.jpg">
-                            @else
-                                <img class="profile-picture" src="/storage/profiles/{{$user->profile_photo}}">
-                            @endif
-                            <li>E-mail address: <a href="mailto:{{$user->email}}">
-                                    {{$user->email}}</a></li>
-                            @if(!is_null($user->phone))
-                                <li>Phone Number: {{$user->phone}}</li>
-                            @endif
-                            <li>Department: {{App\Department::find($user->department_id)->name}}</li>
-                            @if(!is_null($user->profile_url))
-                                @if(strpos($user->profile_url, 'http:\\') !== false)
-                                    <li>Personal URL: <a
-                                                href="{{$user->profile_url}}">{{explode('http:\\\\', $user->profile_url)[1]}}</a>
-                                    </li>
-                                @else
-                                    <li>Personal URL: <a href="http:\\{{$user->profile_url}}">{{$user->profile_url}}</a>
-                                    </li>
-                                @endif
-                            @endif
-                            @if(!is_null($user->presentation))
-                                <li>About me: {{$user->presentation}}</li>
-                            @endif
-                            @can('selfOrAdmin')
-                                <li>Total Requests: {{$user->print_counts}}</li>
-                                <li>Request evaluations: {{$user->print_evals}}</li>
-                                <li>Member for: {{$user->memberFor()}}</li>
-                                <li>Average Satisfaction Grade:
-                                    @for($i=0; $i < $user->averageRating(); ++$i)
-                                        <img src="/star.png" style="width:24px;height:24px;">
-                                    @endfor
-                                </li>
-                                <li>Admin: {{$user->adminToStr()}}</li>
-                                <li>Blocked: {{$user->blockedToStr()}}</li>
-                            @endcan
-                            <br><br>
+                        <div class="profile" id="profile-user">
+                            @include('users.partials.profileInfo')
                         </div>
+                        <br>
                         <div style="float:left;">{{ $requests->links() }}</div>
                         <div class="col-xs-6">
                             <form>
