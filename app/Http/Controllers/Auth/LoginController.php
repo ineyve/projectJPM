36992806aa0522dc04338f4dbe1da911db4d9629
xQsme <2151163@my.ipleiria.pt>
2017-06-03 20:37:06 +0100

    custom messages

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Lang;
 
 class LoginController extends Controller
 {
@@ -46,5 +48,34 @@ class LoginController extends Controller
             'blocked' => 0,
             'activated' => 1
         ];
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        if ( ! User::where('email', $request->email)->first() ) {
+            return redirect()->back()
+                ->withInput($request->only($this->username(), 'remember'))
+                ->withErrors([
+                    $this->username() => Lang::get('auth.email'),
+                ]);
+        }
+        $user=User::where('email', $request->email)->first();
+        if(!$user->activated)
+            return redirect()->back()
+                ->withInput($request->only($this->username(), 'remember'))
+                ->withErrors([
+                    'message' => Lang::get('auth.activated'),
+                ]);
+        if($user->blocked)
+            return redirect()->back()
+                ->withInput($request->only($this->username(), 'remember'))
+                ->withErrors([
+                    'message' => Lang::get('auth.blocked')
+                ]);
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors([
+                'password' => Lang::get('auth.password'),
+            ]);
     }
 }
